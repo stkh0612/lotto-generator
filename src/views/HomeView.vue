20c8215b8ff936906abf003870d38a572894c793
LKH <stkh0612@gmail.com>
2025-05-27 22:12:50 +0900

    기능추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -71,7 +71,7 @@ import AdBanner from '../components/AdBanner.vue'
 
 import axios from 'axios'
 
-const webhook = "https://script.google.com/macros/s/AKfycbz8polqd-f5bRPmqBqlGIzMRoLJ7Eyqx3CBqIf1UPyURC4HgA4gDsEiKfUv67LGORguzA/exec"
+const webhook = "https://superlative-hamster-2861c7.netlify.app/.netlify/functions/proxy"
 
 // 기존 당첨 결과 불러오기
 import lottoResults from '../assets/lotto_numbers_en.json'
